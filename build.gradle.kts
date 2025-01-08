bf5d8eda579325e06a8262794c8c55c22f1e94b0
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2025-01-08 19:02:37 +0100

    fir build native dir

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,26 +19,30 @@ tasks.test {
     useJUnitPlatform()
 }
 
-val nativeDir = "${projectDir}/native"
+val nativeDir: String = file("${projectDir}/native").absolutePath
+val buildNativeDir: String = layout.buildDirectory.dir("native").get().asFile.absolutePath
+
 tasks.withType<JavaCompile> {
     options.compilerArgs.addAll(listOf("-h", nativeDir))
 }
 
-tasks.register("checkCmake") {
+tasks.register("checkNative") {
     doLast {
+        println("Sources dir: $buildNativeDir")
+        println("Build dir: $buildNativeDir")
         println("Checking if CMake is installed...")
         exec {
-            commandLine = listOf("/opt/homebrew/bin/cmake", "--version")
+            commandLine = listOf("cmake", "--version")
         }
     }
 }
 
 tasks.register("configureNative") {
-    dependsOn("compileJava", "checkCmake")
+    dependsOn("compileJava", "checkNative")
     doLast {
         println("Configuring native build using CMake...")
         exec {
-            commandLine = listOf("cmake", "-S", "${nativeDir}/.", "-B", "${nativeDir}/build"/*, "--config", "Release"*/)
+            commandLine = listOf("cmake", "-S", nativeDir, "-B", buildNativeDir)
         }
     }
 }
@@ -48,7 +52,7 @@ tasks.register("buildNative") {
     doLast {
         println("Building native...")
         exec {
-            commandLine = listOf("cmake", "--build", "${nativeDir}/build"/*, "--config", "Release"*/)
+            commandLine = listOf("cmake", "--build", buildNativeDir, "--config", "Release")
         }
     }
 }
