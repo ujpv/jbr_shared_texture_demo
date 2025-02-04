193469b1a40c36a5d8817ce1c8c4df722db4ac4f
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2025-02-04 19:42:11 +0100

    new metal_utils.h

--- native/metal_utils.h
+++ native/metal_utils.h
@@ -6,23 +6,25 @@
 #include <utility>
 
 namespace platform_utils {
-    jlong loadIOSurfaceFromPNG(JNIEnv *env, const std::string &path);
-    jlong getMTLTextureFromIOSurface(JNIEnv *env, jlong pIOSurface);
-    jlong getOpenGLTextureFromIOSurface(JNIEnv *env, jlong pIOSurface);
+//    jlong loadIOSurfaceFromPNG(JNIEnv *env, const std::string &path);
+//    jlong getMTLTextureFromIOSurface(JNIEnv *env, jlong pIOSurface);
+//    jlong getOpenGLTextureFromIOSurface(JNIEnv *env, jlong pIOSurface);
+//
+//    bool createOpenGLContext(JNIEnv *env, jlong sharedContex, jlong pixelFormat);
+//    jlong loadOpenGLTextureFromPNG(JNIEnv *env, const std::string &path);
+//    bool saveOpenGLTextureToPNG(JNIEnv *env, jlong textureId, const std::string& path);
+//
+//    jlong loadMTLTextureFromPNG(JNIEnv *env, const std::string &path);
+//    void releaseMTLTexture(JNIEnv *env, jlong);
+//    jboolean scaleMTLTexture(JNIEnv *env, jlong pSrc, jlong dDst, jdouble scale);
+//    jboolean copyMTLTexture(JNIEnv *env, jlong pSrc, jlong pDst);
+//    jbyteArray MTLTextureToByteArray(JNIEnv *env, jlong ptr);
+//    std::pair<int, int> getMTLTextureSize(JNIEnv *env, jlong ptr);
+//
+//    jlong getTextureFromVolatileImage(JNIEnv *env, jobject vi);
+//    void releaseOpenGLTexture(JNIEnv *env, jlong texture);
 
-    bool createOpenGLContext(JNIEnv *env, jlong sharedContex, jlong pixelFormat);
-    jlong loadOpenGLTextureFromPNG(JNIEnv *env, const std::string &path);
-    bool saveOpenGLTextureToPNG(JNIEnv *env, jlong textureId, const std::string& path);
-
-    jlong loadMTLTextureFromPNG(JNIEnv *env, const std::string &path);
-    void releaseMTLTexture(JNIEnv *env, jlong);
-    jboolean scaleMTLTexture(JNIEnv *env, jlong pSrc, jlong dDst, jdouble scale);
-    jboolean copyMTLTexture(JNIEnv *env, jlong pSrc, jlong pDst);
-    jbyteArray MTLTextureToByteArray(JNIEnv *env, jlong ptr);
-    std::pair<int, int> getMTLTextureSize(JNIEnv *env, jlong ptr);
-
-    jlong getTextureFromVolatileImage(JNIEnv *env, jobject vi);
-    void releaseOpenGLTexture(JNIEnv *env, jlong texture);
+    jboolean renderTriangleToMTLTexture(jlong pTexture);
 }
 
 #endif //NATIVE_METAL_UTILS_H
